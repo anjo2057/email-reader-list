07fdfdeca2f8a9f7a4c1c7e29539065054f17667
anjo2057 <ajohanssond76@gmail.com>
2025-09-25 18:01:34 +0200

    feat: added more pipes

---
+++ src/app/pipes/filter-with-text-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterWithTextPipe } from './filter-with-text-pipe';
+
+describe('FilterWithTextPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterWithTextPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
